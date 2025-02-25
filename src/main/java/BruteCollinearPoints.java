198c78dccd28e50ab6530b9da0705e9c3e55c7aa
26-AMoore <26amoore@derryfield.org>
2025-02-25 14:30:41 -0500

    finished brute force

--- src/main/java/BruteCollinearPoints.java
+++ src/main/java/BruteCollinearPoints.java
@@ -1,27 +1,49 @@
 /*
  *   A program that examines 4 points at a time
- *   and checks whether they all lie on the same line segment, 
- *   returning all such line segments. 
- *   To check whether the 4 points p, q, r, and s are collinear, 
- *   check whether the three slopes between p and q, 
+ *   and checks whether they all lie on the same line segment,
+ *   returning all such line segments.
+ *   To check whether the 4 points p, q, r, and s are collinear,
+ *   check whether the three slopes between p and q,
  *   between p and r, and between p and s are all equal.
  */
+import java.util.ArrayList;
+import edu.princeton.cs.algs4.*;
+
 public class BruteCollinearPoints {
+    private ArrayList<LineSegment> lineSegments = new ArrayList<>();
 
     public BruteCollinearPoints(Point[] points) {
-        // finds all line segments containing 4 points
-        // TODO: YOUR CODE HERE
+
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i + 1; j < points.length; j++) {
+                for (int k = j + 1; k < points.length; k++) {
+                    for (int l = k + 1; l < points.length; l++) {
+                        Point p = points[i];
+                        Point q = points[j];
+                        Point r = points[k];
+                        Point s = points[l];
+
+                        double slopePQ = p.slopeTo(q);
+                        double slopePR = p.slopeTo(r);
+                        double slopePS = p.slopeTo(s);
+
+                        if (slopePQ == slopePR && slopePQ == slopePS) {
+                            Point[] collinearPoints = { p, q, r, s };
+                            java.util.Arrays.sort(collinearPoints);
+                            lineSegments.add(new LineSegment(collinearPoints[0], collinearPoints[3]));
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public int numberOfSegments() {
-        // the number of line segments
-        // TODO: YOUR CODE HERE
-        return 0;
+        return lineSegments.size();
     }
 
     public LineSegment[] segments() {
-        // the line segments
-        // TODO: YOUR CODE HERE
-        return null;
+        return lineSegments.toArray(new LineSegment[0]);
     }
 }
+
